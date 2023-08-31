ef7dab531256a8e7e5f9e502577246c6d7d26f34
serge <sergewilfried@outlook.fr>
2023-08-31 08:56:16 +0000

    feat(useLanguage.ts): add custom hook for fetching and managing language data feat(language.ts): add CRUD operations for language model feat(index.ts): add API routes for managing languages feat(schema.prisma): add Language model and relationships feat(base.ts): add types for Language, Transcript, and AudioTranscript

---
+++ pages/api/lang/[name]/index.ts
@@ -0,0 +1,58 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import {
+  deleteLanguage,
+  getLanguage,
+  updateLanguage,
+} from '../../../../models/language';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { method } = req;
+
+  try {
+    switch (method) {
+      case 'GET':
+        await handleGET(req, res);
+        break;
+      case 'PUT':
+        await handlePUT(req, res);
+        break;
+      case 'DELETE':
+        await handleDelete(req, res);
+        break;
+      default:
+        res.setHeader('Allow', 'GET, PUT, DELETE');
+        res.status(405).json({
+          error: { message: `Method ${method} Not Allowed` },
+        });
+    }
+  } catch (error: any) {
+    const message = error.message || 'Something went wrong';
+    const status = error.status || 500;
+
+    res.status(status).json({ error: { message } });
+  }
+}
+
+const handleGET = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { name } = req.query as { name: string };
+
+  const lang = await getLanguage({ name });
+  res.status(200).json({ data: lang });
+};
+
+const handlePUT = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { name } = req.query as { name: string };
+
+  const lang = await updateLanguage({ name });
+  res.status(200).json({ data: lang });
+};
+
+const handleDelete = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { name } = req.query as { name: string };
+
+  const lang = await deleteLanguage({ name });
+  res.status(200).json({ data: lang });
+};
