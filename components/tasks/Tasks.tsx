d722eb646493e7d08fdeb46afc968ee68f297bf8
serge <sergewilfried@outlook.fr>
2023-10-09 09:03:57 +0000

    refactor(Tasks.tsx): remove unused imports and variables to improve code cleanliness and performance

--- components/tasks/Tasks.tsx
+++ components/tasks/Tasks.tsx
@@ -6,7 +6,6 @@ import { ApiResponse, Task } from 'types';
 import ConfirmationDialog from '../shared/ConfirmationDialog';
 
 import useTasks from '../../hooks/useTasks';
-import { useRouter } from 'next/router';
 import axios from 'axios';
 import toast from 'react-hot-toast';
 import { getAxiosError } from '../../lib/common';
@@ -14,7 +13,6 @@ import { getAxiosError } from '../../lib/common';
 const Tasks = () => {
   const { t } = useTranslation('common');
   const [currentTask, setTeam] = useState<Task | null>(null);
-  const router = useRouter();
   const [askConfirmation, setAskConfirmation] = useState(false);
 
   const { tasks, isLoading, isError, mutateTasks } = useTasks();
