8f73f84e4a25e3853429adc48bf8c2f62ac480d6
serge <sergewilfried@outlook.fr>
2023-10-10 12:04:19 +0000

    chore(minio.ts): add import statement for FetchHttpHandler from "@smithy/fetch-http-handler" to fix missing import error chore(package.json): add "@smithy/fetch-http-handler" dependency to fix missing import error in minio.ts

--- lib/storage/minio.ts
+++ lib/storage/minio.ts
@@ -4,6 +4,7 @@ import {
   S3Client,
 } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { FetchHttpHandler } from '@smithy/fetch-http-handler';
 
 import env from '@/lib/env';
 
