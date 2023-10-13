cf3c8bcf4c3f1a7b2b0c3a837c9b197f7abb3f54
serge <sergewilfried@outlook.fr>
2023-10-13 13:22:19 +0000

    chore(TeamTab.tsx): remove unused ShieldExclamationIcon import to improve code cleanliness

--- components/team/TeamTab.tsx
+++ components/team/TeamTab.tsx
@@ -3,7 +3,6 @@ import {
   DocumentMagnifyingGlassIcon,
   KeyIcon,
   PaperAirplaneIcon,
-  ShieldExclamationIcon,
   UserPlusIcon,
 } from '@heroicons/react/24/outline';
 import type { Team } from '@prisma/client';
