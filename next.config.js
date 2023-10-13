77f61b28c4f2e6ee6fadb6758194cb8eba61bf3c
serge <sergewilfried@outlook.fr>
2023-10-13 16:39:13 +0000

    chore(next.config.js): update image domain from 'akilli.com' to 'boxyhq.com' for consistency chore(next.config.js): comment out unused redirects for '/.well-known/saml.cer' and '/.well-known/saml-configuration'

--- next.config.js
+++ next.config.js
@@ -5,19 +5,19 @@ const { i18n } = require('./next-i18next.config');
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['akilli.com'],
+    domains: ['boxyhq.com'],
   },
   i18n,
   async redirects() {
     return [
-      {
-        source: '/.well-known/saml.cer',
-        destination: '/api/well-known/saml.cer',
-      },
-      {
-        source: '/.well-known/saml-configuration',
-        destination: '/well-known/saml-configuration',
-      },
+      // {
+      //   source: '/.well-known/saml.cer',
+      //   destination: '/api/well-known/saml.cer',
+      // },
+      // {
+      //   source: '/.well-known/saml-configuration',
+      //   destination: '/well-known/saml-configuration',
+      // },
     ];
   },
 };
