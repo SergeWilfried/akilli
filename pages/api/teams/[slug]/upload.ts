f30284c8a6e1cb817ba0389d54d876b80b8cacdf
serge <sergewilfried@outlook.fr>
2023-10-14 18:21:03 +0000

    remove breaking deps

--- pages/api/teams/[slug]/upload.ts
+++ pages/api/teams/[slug]/upload.ts
@@ -1,42 +1,27 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { parseForm } from 'lib/parse-form';
+import { APIRoute, sanitizeKey } from 'next-s3-upload';
+import * as dateFn from 'date-fns';
+import mime from 'mime';
+import env from 'lib/env';
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method !== 'POST') {
-    res.setHeader('Allow', 'POST');
-    res.status(405).json({
-      data: null,
-      error: 'Method Not Allowed',
-    });
-    return;
-  }
-  // Just after the "Method Not Allowed" code
-  try {
-    const { fields, files } = await parseForm(req);
-
-    const file = files?.media;
-    console.warn('media', file);
-    console.warn('fields', fields);
-
-    const url = Array.isArray(file)
-      ? file.map((f) => f.filepath)
-      : file.filepath;
-    res.status(200).json({
-      data: {
-        url,
-      },
-      error: null,
-    });
-  } catch (e: any) {
-    console.error(e);
-    res.status(e?.httpCode || 400).json({ data: null, error: e?.message });
-  }
-};
-
-export const config = {
-  api: {
-    bodyParser: false,
+export default APIRoute.configure({
+  async key(req, filename) {
+    const params = req.body; // 123
+    console.log(params);
+    const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+    const formatedFileName = `${
+      sanitizeKey(filename) || 'unknown'
+    }-${uniqueSuffix}.${
+      mime.getExtension(params?.mimetype || '') || 'unknown'
+    }`;
+    return `/uploads/${dateFn.format(
+      Date.now(),
+      'dd-MM-Y'
+    )}/${formatedFileName}`;
   },
-};
-
-export default handler;
+  endpoint: env.storage.publicEndpoint!,
+  forcePathStyle: true,
+  accessKeyId: env.storage.accessKey,
+  secretAccessKey: env.storage.secretKey,
+  region: env.storage.region,
+  bucket: env.storage.bucketName!,
+});
